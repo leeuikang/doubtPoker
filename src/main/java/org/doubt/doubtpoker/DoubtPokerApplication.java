6e6fc0c42096c7f8c95f408a1f73d1c769cc4ef3
leeuigang <leeuigang@gmail.com>
2026-03-03 14:53:57 +0900

    - DoubtPoker 초기 커밋

---
+++ src/main/java/org/doubt/doubtpoker/DoubtPokerApplication.java
@@ -0,0 +1,13 @@
+package org.doubt.doubtpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubtPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubtPokerApplication.class, args);
+	}
+
+}
