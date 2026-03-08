edcf0b3d4a8f640c690c2781eff25a62909bcf7c
leeuikang <56907030+leeuikang@users.noreply.github.com>
2026-03-08 15:41:59 +0900

    GameController.java <= processJoinRoom method 추가

--- src/main/java/org/doubt/controller/GameController.java
+++ src/main/java/org/doubt/controller/GameController.java
@@ -2,7 +2,9 @@ package org.doubt.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.doubt.dto.GameMessage;
+import org.doubt.handler.SessionManager;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -10,10 +12,21 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class GameController {
     private final SimpMessagingTemplate messagingTemplate;
+    private final SessionManager sessionManager;
 
     @MessageMapping("/game/bet")
     public void processBat(GameMessage message){
         messagingTemplate.convertAndSend("/topic/room" + message.getClass(), message);
+    }
+
+    @MessageMapping("/game/join")
+    public void processJoinRoom(GameMessage message, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("roomId", message.roomId());
+        headerAccessor.getSessionAttributes().put("userName", message.sender());
+
+        sessionManager.addUserToRoom(message.roomId(), message.sender());
+
+        messagingTemplate.convertAndSend("/topic/room/" + message.roomId(), message);
     }
 
 }
