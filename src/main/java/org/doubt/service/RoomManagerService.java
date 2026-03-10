a005dfcc40ba8ce01431e4cc729ebbe65025c7da
leeuigang <leeuigang@gmail.com>
2026-03-10 16:46:17 +0900

    - RoomManagerService.java <= room 관련 서비스 추가

---
+++ src/main/java/org/doubt/service/RoomManagerService.java
@@ -0,0 +1,32 @@
+package org.doubt.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.doubt.repository.PokerRoomRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RoomManagerService {
+
+    private final PokerRoomRepository pokerRoomRepository;
+
+    @Scheduled(fixedDelay =  60 * 1000 * 1000)
+    public void cleanupInactiveRooms(){
+
+        log.info("Cleaning up inactive rooms");
+
+        LocalDateTime threshold = LocalDateTime.now().minusMinutes(10);
+
+        pokerRoomRepository.findAll().forEach(room ->{
+            if(room.getPlayerList().isEmpty() || room.getLastActivityTime().isBefore(threshold)){
+                log.info("Deleting room: {}", room.getRoomId());
+                pokerRoomRepository.deleteById(room.getRoomId());
+            }
+        });
+    }
+}
