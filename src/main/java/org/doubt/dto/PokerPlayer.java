a4d146ddf30398295d5688ecebcaa44efdcf09d1
leeuikang <56907030+leeuikang@users.noreply.github.com>
2026-03-11 14:27:07 +0900

    PokerPlayer.java <= .of 메서드 추가

--- src/main/java/org/doubt/dto/PokerPlayer.java
+++ src/main/java/org/doubt/dto/PokerPlayer.java
@@ -4,7 +4,7 @@ import org.doubt.constant.ChipStatus;
 
 import java.util.List;
 
-public record PokerPlayer(String sessionId, String name, int chips, boolean isReady, List<PokerCard> cardList) {
+public record PokerPlayer(String sessionId, String name, int chips, boolean isReady, List<PokerCard> hand) {
     public static PokerPlayer of(String sessionId, String name) {
         return new PokerPlayer(sessionId, name, ChipStatus.DEFAULT.getValue(), false, new java.util.ArrayList<>());
     }
