44ef9decd82e7c7b7909fb09df16065c1c65ad8f
leeuikang <56907030+leeuikang@users.noreply.github.com>
2026-03-11 15:01:48 +0900

    PokerPlayer.java <= record -> class로 전환

--- src/main/java/org/doubt/dto/PokerPlayer.java
+++ src/main/java/org/doubt/dto/PokerPlayer.java
@@ -1,11 +1,15 @@
 package org.doubt.dto;
 
+import lombok.Getter;
 import org.doubt.constant.ChipStatus;
 
 import java.util.List;
 
-public record PokerPlayer(String sessionId, String name, int chips, boolean isReady, List<PokerCard> hand) {
-    public static PokerPlayer of(String sessionId, String name) {
-        return new PokerPlayer(sessionId, name, ChipStatus.DEFAULT.getValue(), false, new java.util.ArrayList<>());
-    }
+@Getter
+public class PokerPlayer {
+    String sessionId;
+    String name;
+    int chips;
+    boolean isReady;
+    List<PokerCard> hand;
 }
