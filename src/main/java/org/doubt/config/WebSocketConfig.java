051834ee19c8c69f0ebf583fe6d2994f5cd27e5c
leeuigang <leeuigang@gmail.com>
2026-04-03 10:48:35 +0900

    - WebSocketConfig.java <= log 관련 interceptor 추가

--- src/main/java/org/doubt/config/WebSocketConfig.java
+++ src/main/java/org/doubt/config/WebSocketConfig.java
@@ -1,12 +1,20 @@
 package org.doubt.config;
 
+import lombok.RequiredArgsConstructor;
+import org.doubt.interceptor.StompLogInterceptor;
+import org.doubt.interceptor.StompOutboundLogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-@EnableWebSocketMessageBroker
 @Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompLogInterceptor stompLogInterceptor;
+    private final StompOutboundLogInterceptor stompOutboundLogInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -18,5 +26,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompLogInterceptor);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompOutboundLogInterceptor);
     }
 }
